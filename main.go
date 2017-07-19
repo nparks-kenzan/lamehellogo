6d50e491db6dfa4f916fbf3d366ad9053f80fe37
Nicholas J. Parks <nparks@kenzan.com>
2017-07-19 15:16:07 -0400

    lamer

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "I guess we can be cliche and use the all too popular - Hello World")
+	io.WriteString(w, "Hello World")
 }
 
 func main() {
