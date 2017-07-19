3e5b090723343845e34a5e26f708cc00e8a0ce4b
Nicholas J. Parks <nparks@kenzan.com>
2017-07-19 15:02:16 -0400

    lame

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "I guess we can be cliche and use the all too popular - Hello Simulated World")
+	io.WriteString(w, "I guess we can be cliche and use the all too popular - Hello World")
 }
 
 func main() {
